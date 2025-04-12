082adc3e24226f4f9adb3d5ce116121f05188c2e
dzelapino <kolodziejskikrzysztofhubert@gmail.com>
2025-04-12 20:12:31 +0200

    - Dodanie service worker

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/ngsw-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope: ', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed: ', error);
+    });
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
